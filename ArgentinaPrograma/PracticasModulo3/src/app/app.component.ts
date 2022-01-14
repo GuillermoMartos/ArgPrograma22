b95c11e7fb38cd9c92e0b5f11e2ddc11afc4ddca
Guillermo L. Martos <guille.l.martos@gmail.com>
2022-01-14 19:19:03 -0300

    clase video

--- ArgentinaPrograma/PracticasModulo3/src/app/app.component.ts
+++ ArgentinaPrograma/PracticasModulo3/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PracticasModulo3';
+  title : string = 'PracticasModulo3';
 }

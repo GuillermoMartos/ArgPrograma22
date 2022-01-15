c1f5cb77e7a1dc51a1c40ab5a6df4530edb7a16d
Guillermo L. Martos <guille.l.martos@gmail.com>
2022-01-15 11:14:26 -0300

    css y ng  service

---
+++ ArgentinaPrograma/PracticasModulo3/src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/service/task.service';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks:Task[]=[];
+  constructor(private taskService:TaskService) {
+    
+   }
+
+  ngOnInit(): void {
+    this.tasks=this.taskService.getTasks()
+  }
+
+}
